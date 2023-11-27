f9dbace059c7c75ade8a65b0c84d6a676ca4cc77
Pavel.V <vaninen@ya.ru>
2023-11-28 00:55:55 +0300

    hw2

--- Paradigma/HW2/hw2.py
+++ Paradigma/HW2/hw2.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def table_multiplication(number):
+    for i in range(1, number + 1):
+        for j in range(1, 10):
+            print(f"{i} * {j} = {i * j}")
+        print('')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    num = int(input("Введите число n: "))
+    table_multiplication(num)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# В данном примере выбрана процедурная парадигма, так как она является
+# простой и понятной для решения этой задачи.
